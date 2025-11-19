1c45b2f7b7478bc3c92646cb4c131df0e8c410c3
GTO浪歌 <15415159+GTOLonger@user.noreply.gitee.com>
2025-11-19 14:02:02 +0800

    Add 新增玩家属性查看HUD Modify 修改目标属性查看HUD，适配框架组件卸载事件更新

--- Assets/Scripts/MVVM/ViewModels/HeroAttributeViewModel.cs
+++ Assets/Scripts/MVVM/ViewModels/HeroAttributeViewModel.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Classes;
 using Hero;
 using TMPro;
 using Unity.VisualScripting;
@@ -8,8 +9,9 @@ using UnityEngine;
 
 namespace MVVM.ViewModels
 {
-    public class TargetAttributeViewModel : MonoBehaviour
+    public class HeroAttributeViewModel : MonoBehaviour
     {
+        
         private Transform _attributeList;
         private event Action UnBindEvent; 
         
@@ -17,21 +19,26 @@ namespace MVVM.ViewModels
         {
             Dictionary<string, TMP_Text> textGroup = new Dictionary<string, TMP_Text>();
             
-            _attributeList = transform.Find("Background/AttributeList");
+            _attributeList = transform.Find("BasicAttributesBackground/AttributeList");
+            for (int index = 0; index < _attributeList.childCount;index++)
+            {
+                textGroup[_attributeList.GetChild(index).name] = _attributeList.GetChild(index).Find("Content").GetComponent<TextMeshProUGUI>();
+            }
+            _attributeList = transform.Find("OtherAttributesBackground/AttributeList");
             for (int index = 0; index < _attributeList.childCount;index++)
             {
                 textGroup[_attributeList.GetChild(index).name] = _attributeList.GetChild(index).Find("Content").GetComponent<TextMeshProUGUI>();
             }
             
-            UnBindEvent += Binder.BindActive(gameObject, HeroManager.hero.target);
-            UnBindEvent += Binder.BindTextGroup(textGroup, HeroManager.hero.target);
+            UnBindEvent += Binder.BindTextGroup(textGroup, HeroManager.hero);
+            UnBindEvent += Binder.BindActive(gameObject, HeroManager.hero.showAttributes);
         }
 
         void Update()
         {
             
         }
-        
+
         // 物体销毁时触发注销对应事件
         void OnDestroy()
         {

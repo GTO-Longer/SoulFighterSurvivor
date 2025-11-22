5d717e4340dea735720e5b280eace5020c770f13
GTO浪歌 <15415159+GTOLonger@user.noreply.gitee.com>
2025-11-22 14:20:27 +0800

    Add 增加了事件系统，增加了持续更新事件、QWER技能释放事件等技能系统相关的常用事件

--- Assets/Scripts/Systems/EventSystem.cs
+++ Assets/Scripts/Systems/EventSystem.cs
@@ -1,18 +1,60 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Classes;
 using UnityEngine;
 
-public class EventSystem : MonoBehaviour
+namespace Systems
 {
-    // Start is called before the first frame update
-    void Start()
+    public class EventSystem
     {
+        // 持续更新事件
+        public event Action<Entity, Entity> EntityUpdateEvent;
+        public void UpdateEvent(Entity owner)
+        {
+            EntityUpdateEvent?.Invoke(owner, null);
+        }
         
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
+        // Q技能释放事件
+        public event Action<Entity, Entity> OnQSkillRelease;
+        public void QSkillRelease(Entity owner, Entity target)
+        {
+            OnQSkillRelease?.Invoke(owner, target);
+        }
         
+        // W技能释放事件
+        public event Action<Entity, Entity> OnWSkillRelease;
+        public void WSkillRelease(Entity owner, Entity target)
+        {
+            OnWSkillRelease?.Invoke(owner, target);
+        }
+        
+        // E技能释放事件
+        public event Action<Entity, Entity> OnESkillRelease;
+        public void ESkillRelease(Entity owner, Entity target)
+        {
+            OnESkillRelease?.Invoke(owner, target);
+        }
+        
+        // R技能释放事件
+        public event Action<Entity, Entity> OnRSkillRelease;
+        public void RSkillRelease(Entity owner, Entity target)
+        {
+            OnRSkillRelease?.Invoke(owner, target);
+        }
+        
+        // 击杀单位
+        public event Action<Entity, Entity> OnKillEntity;
+        public void KillEntity(Entity owner, Entity target)
+        {
+            OnKillEntity?.Invoke(owner, target);
+        }
+        
+        // 技能命中
+        public event Action<Entity, Entity> OnSkillHit;
+        public void SkillHit(Entity owner, Entity target)
+        {
+            OnSkillHit?.Invoke(owner, target);
+        }
     }
 }

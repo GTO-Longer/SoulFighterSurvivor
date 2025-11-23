31c774af4230bba5e649fda6a50c4cbfefa21c03
GTO浪歌 <15415159+GTOLonger@user.noreply.gitee.com>
2025-11-23 21:25:51 +0800

    Add 新增子弹工厂类，单例模式+工厂模式控制子弹对象生成

--- Assets/Scripts/Classes/Bullet.cs
+++ Assets/Scripts/Classes/Bullet.cs
@@ -1,18 +1,47 @@
-using System.Collections;
-using System.Collections.Generic;
+using System;
 using UnityEngine;
+using Utilities;
 
-public class Bullet : MonoBehaviour
+namespace Classes
 {
-    // Start is called before the first frame update
-    void Start()
+    public class Bullet
     {
+        public GameObject gameObject;
+        private Entity _owner;
+        private Team _team;
+        private float _bulletContinuousTime;
+        private float _bulletContinuousTimer;
         
-    }
+        public event Action<Bullet> BulletAwake;
+        public event Action<Bullet> BulletUpdate;
+        public event Action<Bullet> BulletDestroy;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        internal Bullet(Entity owner, GameObject bulletPrefab)
+        {
+            _owner = owner;
+            gameObject = GameObject.Instantiate(bulletPrefab, bulletPrefab.transform.parent);
+            _team = owner.team;
+        }
+
+        public void Awake()
+        {
+            BulletAwake?.Invoke(this);
+        }
+
+        public void Update()
+        {
+            BulletUpdate?.Invoke(this);
+        }
+
+        public void Destroy()
+        {
+            BulletDestroy?.Invoke(this);
+            
+            BulletAwake = null;
+            BulletUpdate = null;
+            BulletDestroy = null;
+            
+            GameObject.Destroy(gameObject);
+        }
     }
 }
---
+++ Assets/Scripts/Factories/BulletFactory.cs
@@ -0,0 +1,54 @@
+using System.Collections.Generic;
+using UnityEngine;
+using Classes;
+using Utilities;
+
+namespace Factories
+{
+    public class BulletFactory : MonoBehaviour
+    {
+        public int ActiveBulletCount => _activeBullets.Count;
+        public static BulletFactory Instance { get; private set; }
+        public GameObject bulletPrefab;
+
+        private readonly List<Bullet> _activeBullets = new List<Bullet>();
+
+        private void Awake()
+        {
+            if (Instance != null && Instance != this)
+            {
+                Destroy(gameObject);
+                return;
+            }
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+            
+            bulletPrefab.SetActive(false);
+        }
+
+        public Bullet CreateBullet(Entity owner)
+        {
+            var bullet = new Bullet(owner, bulletPrefab);
+            _activeBullets.Add(bullet);
+            return bullet;
+        }
+
+        private void Update()
+        {
+            for (int i = _activeBullets.Count - 1; i >= 0; i--)
+            {
+                var bullet = _activeBullets[i];
+                bullet.Update();
+            }
+        }
+
+        public void ClearAllBullets()
+        {
+            foreach (var bullet in _activeBullets)
+            {
+                bullet.Destroy();
+            }
+            _activeBullets.Clear();
+        }
+    }
+}

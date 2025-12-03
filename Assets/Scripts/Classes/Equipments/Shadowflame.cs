c13f50fe6571647a3859d8a310b4e4901f511d64
GTO浪歌 <15415159+GTOLonger@user.noreply.gitee.com>
2025-12-03 14:00:48 +0800

    Add 实现装备影焰的装备效果，修改了装备的效果描述

--- Assets/Scripts/Classes/Equipments/Shadowflame.cs
+++ Assets/Scripts/Classes/Equipments/Shadowflame.cs
@@ -1,10 +1,39 @@
+using System;
+using Managers.EntityManagers;
+using Utilities;
+
 namespace Classes.Equipments
 {
     public class Shadowflame : Equipment
     {
+        private Action<Entity, Entity, float> equipmentEffect;
+        
         public Shadowflame() : base("Shadowflame")
         {
+            equipmentEffect = (attacker, target, damageCount) =>
+            {
+                target.TakeDamage(damageCount * 0.15f, DamageType.Real, attacker);
+            };
+        }
+
+        public override void OnEquipmentGet(Entity entity)
+        {
+            base.OnEquipmentGet(entity);
+
+            owner.AbilityEffect += equipmentEffect;
+        }
+
+        public override void OnEquipmentRemove()
+        {
+            base.OnEquipmentRemove();
             
+            owner.AbilityEffect -= equipmentEffect;
+        }
+
+        public override bool GetPassiveSkillDescription(out string description)
+        {
+            description = string.Format(_passiveSkillName + "\n" + _passiveSkillDescription);
+            return true;
         }
     }
 }

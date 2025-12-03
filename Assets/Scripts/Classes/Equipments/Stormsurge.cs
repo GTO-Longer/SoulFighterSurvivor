e3c016ecac778443a542cd6a4fa07a089f0fa35e
GTO浪歌 <15415159+GTOLonger@user.noreply.gitee.com>
2025-12-03 13:44:48 +0800

    Add 实现装备风暴狂涌的装备技能效果

--- Assets/Scripts/Classes/Equipments/Stormsurge.cs
+++ Assets/Scripts/Classes/Equipments/Stormsurge.cs
@@ -1,10 +1,50 @@
+using System;
+using Managers.EntityManagers;
+using Utilities;
+
 namespace Classes.Equipments
 {
     public class Stormsurge : Equipment
     {
+        private float damageCount => 180 + 0.5f * HeroManager.hero.abilityPower;
+        private Action<Entity, Entity, float> equipmentEffect;
+        private float damageSum;
+        
         public Stormsurge() : base("Stormsurge")
         {
+            equipmentEffect = (attacker, target, damageCount) =>
+            {
+                damageSum += damageCount;
+                Async.SetAsync(2.5f, null, null, () => damageSum -= damageCount);
+
+                if (damageSum >= target.maxHealthPoint.Value * 0.25f)
+                {
+                    Async.SetAsync(2, null, null, () => target.TakeDamage(target.CalculateAPDamage(attacker, damageCount), DamageType.AP, attacker));
+                    _passiveSkillCDTimer = 0;
+                }
+            };
+        }
+
+        public override void OnEquipmentGet(Entity entity)
+        {
+            base.OnEquipmentGet(entity);
+
+            owner.EntityUpdateEvent += equipmentTimerUpdate;
+            owner.AbilityEffect += equipmentEffect;
+        }
+
+        public override void OnEquipmentRemove()
+        {
+            base.OnEquipmentRemove();
             
+            owner.EntityUpdateEvent -= equipmentTimerUpdate;
+            owner.AbilityEffect -= equipmentEffect;
+        }
+
+        public override bool GetPassiveSkillDescription(out string description)
+        {
+            description = string.Format(_passiveSkillName + "\n" + _passiveSkillDescription, damageCount);
+            return true;
         }
     }
 }

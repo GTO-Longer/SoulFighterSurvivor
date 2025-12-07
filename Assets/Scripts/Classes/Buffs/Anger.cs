11dc23de2e7914609966296b5263301cedf0418c
GTO浪歌 <15415159+GTOLonger@user.noreply.gitee.com>
2025-12-07 19:05:59 +0800

    Add 实现“毁坏仪式”的装备效果

---
+++ Assets/Scripts/Classes/Buffs/Anger.cs
@@ -0,0 +1,30 @@
+using System;
+using UnityEngine;
+
+namespace Classes.Buffs
+{
+    public class Anger : Buff
+    {
+        public Anger(Entity ownerEntity, Entity sourceEntity) : base(ownerEntity, sourceEntity, "怒火", "增加暴击率", 10, 6)
+        {
+            OnBuffGet = () =>
+            {
+                if (buffCount < buffMaxCount)
+                {
+                    buffCount += 1;
+                    buffDescription = $"增加{0.025f * buffCount:P0}暴击率";
+                }
+
+                owner._criticalRateBonus.Value += 0.025f;
+            };
+            
+            OnBuffRunOut = () => 
+            {
+                owner._criticalRateBonus.Value -= 0.025f * buffCount;
+                buffCount = 0;
+            };
+
+            isUnique = true;
+        }
+    }
+}
---
+++ Assets/Scripts/Classes/Equipments/Ruinous_Ritual.cs
@@ -0,0 +1,46 @@
+using System;
+using Managers.EntityManagers;
+using Utilities;
+using Random = UnityEngine.Random;
+
+namespace Classes.Equipments
+{
+    public class Ruinous_Ritual : Equipment
+    {
+        private Action<Entity, Entity, float, Skill> equipmentEffect;
+        private float damageCount => 70 + 0.15f * HeroManager.hero.abilityPower;
+        
+        public Ruinous_Ritual() : base("Ruinous_Ritual")
+        {
+            equipmentEffect = (attacker, target, _, _) =>
+            {
+                var anger = new Buffs.Anger(attacker, attacker);
+                attacker.GetBuff(anger);
+
+                if (Random.Range(0f, 1f) <= owner.criticalRate)
+                {
+                    target.TakeDamage(target.CalculateAPDamage(attacker, damageCount), DamageType.AP, attacker);
+                    _passiveSkillCDTimer = 0;
+                }
+            };
+        }
+
+        public override void OnEquipmentGet(Entity entity)
+        {
+            base.OnEquipmentGet(entity);
+            owner.AbilityEffect += equipmentEffect;
+        }
+
+        public override void OnEquipmentRemove()
+        {
+            owner.AbilityEffect -= equipmentEffect;
+            base.OnEquipmentRemove();
+        }
+
+        public override bool GetPassiveSkillDescription(out string description)
+        {
+            description = string.Format(_passiveSkillName + "\n" + _passiveSkillDescription, damageCount);
+            return true;
+        }
+    }
+}

641aed97f4c253087429ff310fb40e9920254643
GTO浪歌 <15415159+GTOLonger@user.noreply.gitee.com>
2025-12-07 18:06:10 +0800

    Add 实现“灭世者的死亡之帽”的装备效果

---
+++ Assets/Scripts/Classes/Equipments/Rabadons_Deathcap.cs
@@ -0,0 +1,28 @@
+namespace Classes.Equipments
+{
+    public class Rabadons_Deathcap : Equipment
+    {
+        public Rabadons_Deathcap() : base("Rabadons_Deathcap")
+        {
+            
+        }
+
+        public override void OnEquipmentGet(Entity entity)
+        {
+            base.OnEquipmentGet(entity);
+            entity._percentageAbilityPowerBonus.Value += 0.3f;
+        }
+
+        public override void OnEquipmentRemove()
+        {
+            owner._percentageAbilityPowerBonus.Value -= 0.3f;
+            base.OnEquipmentRemove();
+        }
+
+        public override bool GetPassiveSkillDescription(out string description)
+        {
+            description = string.Format(_passiveSkillName + "\n" + _passiveSkillDescription);
+            return true;
+        }
+    }
+}

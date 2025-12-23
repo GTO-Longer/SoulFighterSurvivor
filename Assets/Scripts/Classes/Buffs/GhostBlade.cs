5bf0ebda7a50c8a3d068d31afbf6d5dbb51b0018
GTO浪歌 <15415159+GTOLonger@user.noreply.gitee.com>
2025-12-23 23:11:46 +0800

    Add 添加“幽梦之灵”装备效果

---
+++ Assets/Scripts/Classes/Buffs/GhostBlade.cs
@@ -0,0 +1,34 @@
+using System;
+
+namespace Classes.Buffs
+{
+    public class GhostBlade : Buff
+    {
+        private Action<Entity> Ghost;
+        public GhostBlade(Entity ownerEntity, Entity sourceEntity) : base(ownerEntity, sourceEntity, "幽梦之灵", "移动速度增加30%，进入幽灵状态。", 0, 6)
+        {
+            Ghost = (_) =>
+            {
+                if (owner != null)
+                {
+                    owner.agent.isGoust = true;
+                }
+            };
+            
+            OnBuffGet = () =>
+            {
+                owner._percentageMovementSpeedBonus.Value += 0.3f;
+                owner.EntityUpdateEvent += Ghost;
+            };
+            
+            OnBuffRunOut = () => 
+            {
+                owner._percentageMovementSpeedBonus.Value -= 0.3f;
+                owner.EntityUpdateEvent -= Ghost;
+                owner.agent.isGoust = false;
+            };
+
+            isUnique = true;
+        }
+    }
+}
---
+++ Assets/Scripts/Classes/Equipments/Youmuu_sGhostblade.cs
@@ -0,0 +1,47 @@
+using System;
+using UnityEngine;
+using Utilities;
+
+namespace Classes.Equipments
+{
+    public class Youmuu_sGhostblade : Equipment
+    {
+        public Youmuu_sGhostblade() : base("Youmuu_sGhostblade")
+        {
+            ActiveSkillEffective = () =>
+            {
+                if (!_activeSkillActive) return;
+                
+                _activeSkillCDTimer = 0;
+                var goustBlade = new Buffs.GhostBlade(owner, owner);
+                owner.GetBuff(goustBlade);
+            };
+        }
+
+        public override void OnEquipmentGet(Entity entity)
+        {
+            base.OnEquipmentGet(entity);
+            owner._movementSpeedBonus.Value += 60;
+            owner.EntityUpdateEvent += equipmentTimerUpdate;
+        }
+
+        public override void OnEquipmentRemove()
+        {
+            owner._movementSpeedBonus.Value -= 60;
+            owner.EntityUpdateEvent -= equipmentTimerUpdate;
+            base.OnEquipmentRemove();
+        }
+
+        public override bool GetPassiveSkillDescription(out string description)
+        {
+            description = string.Format(_passiveSkillName + "\n" + _passiveSkillDescription);
+            return true;
+        }
+
+        public override bool GetActiveSkillDescription(out string description)
+        {
+            description = string.Format(_activeSkillName + "\n" + _activeSkillDescription);
+            return true;
+        }
+    }
+}

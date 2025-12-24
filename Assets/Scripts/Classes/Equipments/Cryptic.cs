00bd92a8912b4ff0822379cf4986aa1ab82f4e54
GTO浪歌 <15415159+GTOLonger@user.noreply.gitee.com>
2025-12-24 13:45:31 +0800

    Modify 修改装备“困惑”的增伤作用范围

--- Assets/Scripts/Classes/Equipments/Cryptic.cs
+++ Assets/Scripts/Classes/Equipments/Cryptic.cs
@@ -6,10 +6,20 @@ namespace Classes.Equipments
     public class Cryptic : Equipment
     {
         private Action<Entity, Entity, float, Skill> equipmentEffect;
+        private Action<Entity, Entity, float, float> equipmentEffect2;
         
         public Cryptic() : base("Cryptic")
         {
             equipmentEffect = (attacker, target, damageCount, _) =>
+            {
+                if (target.maxHealthPoint >= attacker.maxHealthPoint)
+                {
+                    var ratio = 0.05f + (target.maxHealthPoint - attacker.maxHealthPoint) * 0.00004f;
+                    target.TakeDamage(damageCount * ratio, DamageType.Real, attacker);
+                }
+            };
+            
+            equipmentEffect2 = (attacker, target, damageCount, _) =>
             {
                 if (target.maxHealthPoint >= attacker.maxHealthPoint)
                 {
@@ -23,11 +33,13 @@ namespace Classes.Equipments
         {
             base.OnEquipmentGet(entity);
             owner.AbilityEffect += equipmentEffect;
+            owner.AttackEffect += equipmentEffect2;
         }
 
         public override void OnEquipmentRemove()
         {
             owner.AbilityEffect -= equipmentEffect;
+            owner.AttackEffect -= equipmentEffect2;
             base.OnEquipmentRemove();
         }
 

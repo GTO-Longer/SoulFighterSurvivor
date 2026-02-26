f244ddf17607beda856f67a0c973efb1c87a026f
GTO浪歌 <15415159+GTOLonger@user.noreply.gitee.com>
2026-02-26 22:11:30 +0800

    Add 实现“海克斯注力钢壁”的装备效果

---
+++ Assets/Scripts/Classes/Buffs/Hullbreaker.cs
@@ -0,0 +1,25 @@
+using System;
+using UnityEngine;
+
+namespace Classes.Buffs
+{
+    public class Hullbreaker : Buff
+    {
+        public Hullbreaker(Entity ownerEntity, Entity sourceEntity) : base(ownerEntity, sourceEntity, "过载", "获得50%总攻击速度", 0, 10)
+        {
+            OnBuffGet = () =>
+            {
+                owner._percentageAttackSpeedBonus.Value += 0.5f;
+                owner._percentageMovementSpeedBonus.Value += 0.2f;
+            };
+            
+            OnBuffRunOut = () => 
+            {
+                owner._percentageAttackSpeedBonus.Value -= 0.5f;
+                owner._percentageMovementSpeedBonus.Value -= 0.2f;
+            };
+
+            isUnique = true;
+        }
+    }
+}
---
+++ Assets/Scripts/Classes/Equipments/Hullbreaker.cs
@@ -0,0 +1,45 @@
+using System;
+using Utilities;
+
+namespace Classes.Equipments
+{
+    public class Hullbreaker : Equipment
+    {
+        private Action<Skill> equipmentEffect;
+        
+        public Hullbreaker() : base("Hullbreaker")
+        {
+            equipmentEffect = (skill) =>
+            {
+                if (_passiveSkillActive && skill.skillType == SkillType.RSkill)
+                {
+                    var burnt = new Buffs.Hullbreaker(owner, owner);
+                    burnt.buffIcon = equipmentIcon;
+                    owner.GainBuff(burnt);
+                    
+                    _passiveSkillCDTimer = 0;
+                }
+            };
+        }
+        
+        public override void OnEquipmentGet(Entity entity)
+        {
+            base.OnEquipmentGet(entity);
+            owner.OnSkillUsed += equipmentEffect;
+            owner.EntityUpdateEvent += equipmentTimerUpdate;
+        }
+        
+        public override void OnEquipmentRemove()
+        {
+            owner.OnSkillUsed -= equipmentEffect;
+            owner.EntityUpdateEvent -= equipmentTimerUpdate;
+            base.OnEquipmentRemove();
+        }
+        
+        public override bool GetPassiveSkillDescription(out string description)
+        {
+            description = string.Format(_passiveSkillName + "\n" + _passiveSkillDescription);
+            return true;
+        }
+    }
+}

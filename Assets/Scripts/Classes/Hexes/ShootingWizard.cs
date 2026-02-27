e0bbdb11dfd03dd93fe7d0b1a9c1c1111dca2a13
GTO浪歌 <15415159+GTOLonger@user.noreply.gitee.com>
2026-02-28 01:26:50 +0800

    Add 实现“神射法师”海克斯效果

---
+++ Assets/Scripts/Classes/Hexes/ShootingWizard.cs
@@ -0,0 +1,44 @@
+using System;
+using Classes.Entities;
+using Factories;
+using UnityEngine;
+using Utilities;
+using Random = UnityEngine.Random;
+
+namespace Classes.Hexes
+{
+    public class ShootingWizard : Hex
+    {
+        private Action<Entity, Entity, bool> HexEffect;
+        private float damageSum = 0;
+        
+        public ShootingWizard() : base("ShootingWizard")
+        {
+            HexEffect = (_, target, isCrit) =>
+            {
+                // 计算攻击伤害
+                var damageCount = target.CalculateADDamage(owner, owner.abilityPower);
+                target.TakeDamage(damageCount, DamageType.AD, owner, isCrit);
+                damageSum += damageCount;
+            };
+        }
+
+        public override void OnHexGet(Entity entity)
+        {
+            base.OnHexGet(entity);
+            owner.OnAttackHit += HexEffect;
+        }
+
+        public override void OnHexRemove()
+        {
+            owner.OnAttackHit -= HexEffect;
+            base.OnHexRemove();
+        }
+
+        public override bool GetHexDetail(out string detail)
+        {
+            detail = string.Format(hexDetail, damageSum);
+            return true;
+        }
+    }
+}

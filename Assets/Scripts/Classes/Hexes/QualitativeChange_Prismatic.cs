32b75b1861edda9ad4dd64588ac8e07d73792d02
GTO浪歌 <15415159+GTOLonger@user.noreply.gitee.com>
2026-02-28 01:23:52 +0800

    Add 实现“质变：棱彩阶”海克斯效果

---
+++ Assets/Scripts/Classes/Hexes/QualitativeChange_Prismatic.cs
@@ -0,0 +1,35 @@
+using Managers;
+using Managers.EntityManagers;
+using UnityEngine;
+using Utilities;
+
+namespace Classes.Hexes
+{
+    public class QualitativeChange_Prismatic : Hex
+    {
+        public QualitativeChange_Prismatic() : base("QualitativeChange_Prismatic")
+        {
+            
+        }
+
+        public override void OnHexGet(Entity entity)
+        {
+            canChoose = false;
+            HeroManager.hero.RemoveHex(this);
+            ToolFunctions.GetRandomUniqueItems(HexManager.Instance.hexList.FindAll(hex => hex.canChoose && !HeroManager.hero.hexList.Contains(hex) && hex.hexQuality == Quality.Prismatic), 1, out var results);
+            Debug.Log("已质变：" + results[0].hexName);
+            HeroManager.hero.GetHex(results[0]);
+        }
+
+        public override void OnHexRemove()
+        {
+            
+        }
+
+        public override bool GetHexDetail(out string detail)
+        {
+            detail = string.Format(hexDetail);
+            return true;
+        }
+    }
+}

a611f8b18f4bc7aa1e941e6cf6119b87c74030d4
GTO浪歌 <15415159+GTOLonger@user.noreply.gitee.com>
2026-02-28 01:02:58 +0800

    Add 实现“属性！”海克斯效果

---
+++ Assets/Scripts/Classes/Hexes/Attributes.cs
@@ -0,0 +1,28 @@
+using System;
+using Classes.Entities;
+using Managers;
+using MVVM.ViewModels;
+
+namespace Classes.Hexes
+{
+    public class Attributes : Hex
+    {
+        public Attributes() : base("Attributes")
+        {
+
+        }
+
+        public override void OnHexGet(Entity entity)
+        {
+            base.OnHexGet(entity);
+            EquipmentManager.Instance.GetEquipment("AttributeAnvil").OnActiveSkillEffective();
+            EquipmentManager.Instance.GetEquipment("AttributeAnvil").OnActiveSkillEffective();
+        }
+
+        public override bool GetHexDetail(out string detail)
+        {
+            detail = string.Format(hexDetail);
+            return true;
+        }
+    }
+}

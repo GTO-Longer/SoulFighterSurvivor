427fc3c99c4c982009b033695a49df752053fa9c
GTO浪歌 <15415159+GTOLonger@user.noreply.gitee.com>
2026-05-19 16:24:58 +0800

    Modify 测试系统新增单项寻路测试，旨在测试单NavMesh寻路下的寻路情况和性能比较。 这期神了

--- Assets/Scripts/Systems/TestSystem.cs
+++ Assets/Scripts/Systems/TestSystem.cs
@@ -1,3 +1,6 @@
+using System.Linq;
+using Classes.Buffs;
+using DataManagement;
 using Managers.EntityManagers;
 using UnityEngine;
 
@@ -11,6 +14,7 @@ namespace Systems
         public bool SkillNoCoolDown;
         public bool LevelUp;
         public bool Gain20000Coin;
+        public bool PathFindTest;
 
         private void Start()
         {
@@ -55,6 +59,26 @@ namespace Systems
             {
                 HeroManager.hero.coins.Value += 20000;
                 Gain20000Coin = false;
+            }
+
+            if (PathFindTest)
+            {
+                var entities = GameObject.FindObjectsOfType<EntityData>()
+                    .Select(e => e.entity)
+                    .Where(e => e != null && e.isAlive)
+                    .ToList();
+
+                foreach (var entity in entities)
+                {
+                    var ghostPoro = new GhostPoro(entity, entity);
+                    ghostPoro.buffIcon = null;
+                    entity.GainBuff(ghostPoro);
+                    
+                    for (var j = 0; j < 20000 / Mathf.Max(1, entities.Count - 1); j++)
+                    {
+                        var x = Mathf.Sin(j * 0.001f) * Mathf.Cos(j * 0.002f) * Mathf.Asin(entities.Count * 0.1145141919810f);
+                    }
+                }
             }
         }
     }
